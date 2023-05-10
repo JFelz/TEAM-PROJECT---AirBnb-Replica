db1eec6c6dfd3fe57342ba95b5f268532cfe353d
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-05-09 20:22:42 -0500

    GRID

--- components/ProfileImage.js
+++ components/ProfileImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ProfileImg() {
   return (
     <div>
-      <img src="/public/UserProfileImage.png" alt="Profile Img" />
+      Hi
     </div>
   );
 }
